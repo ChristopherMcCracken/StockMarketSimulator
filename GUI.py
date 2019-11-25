f7170caa4176de6de3126146810c0bd5e992c69f
Christopher McCracken <46425436+Chrithtoph@users.noreply.github.com>
2019-11-25 12:55:11 -0700

    Minor GUI Changes to Home Screen

--- GUI.py
+++ GUI.py
@@ -1,4 +1,3 @@
-import PySide2
 from PySide2 import QtCore, QtWidgets
 from PySide2.QtGui import QPalette, QColor, Qt, QFont
 from PySide2.QtWidgets import QMainWindow, QInputDialog, QWidget, QPushButton, QFormLayout, QLineEdit, QLabel
@@ -7,7 +6,6 @@ from Main import createPortfolio
 import sys
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import GrabDataFromAPI
 
 
@@ -40,7 +38,7 @@ class Ui_Application(object):
         label = QtWidgets.QLabel(self.viewPortfolioWindow)
         label.move(50, 50)
         label.setText(Main.getPortfolioInfo(portfolioName))
-        label.setStyleSheet("QLabel {font: 16pt Calibri}")
+        label.setStyleSheet("QLabel {font: 20pt Calibri}")
         label.adjustSize()
         self.viewPortfolioWindow.show()
 
@@ -71,57 +69,94 @@ class Ui_Application(object):
         grid.addWidget(canvas, 0, 0)
         self.wid.show()
 
-    # ---------------------------------------------------------------------------------------------------------------- #
+    # ------------------------------------------------------------------------------------------------------------ #
     def setupUi(self, Application):
         Application.setObjectName("Application")
-        Application.resize(2000, 2000)
-
-        labelA = QtWidgets.QLabel(Application)
-        labelA.setText('Virtual Stock Market Application')
-        labelA.setStyleSheet("QLabel {font: 32pt Elephant; color: #e8fcca}")
-        labelA.adjustSize()
-        labelA.move(125, 60)
+        Application.resize(1000, 1000)
 
         self.gridLayout = QtWidgets.QGridLayout(Application)
         self.gridLayout.setObjectName("gridLayout")
 
-        self.pushButton_2 = QtWidgets.QPushButton(Application)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 4, 0, 1, 1)
-        self.pushButton_2.setStyleSheet('QPushButton {background-color: #525151; color: white; font: 70px Elephant; }')
+        # Top Label
+        self.label = QtWidgets.QLabel(Application)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.label.setStyleSheet("QLabel {font: 30pt Elephant; color: #e8fcca}")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
-        self.pushButton_3 = QtWidgets.QPushButton(Application)
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.gridLayout.addWidget(self.pushButton_3, 0, 0, 1, 1)
-        self.pushButton_3.setStyleSheet('QPushButton {background-color: #525151; color: white; font: 70px Elephant;}')
-
+        # First Button
         self.pushButton = QtWidgets.QPushButton(Application)
-        self.pushButton.setStyleSheet("")
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.setStyleSheet('QPushButton {font: 25pt Elephant}')
         self.gridLayout.addWidget(self.pushButton, 2, 0, 1, 1)
-        self.pushButton.setStyleSheet('QPushButton {background-color: #525151; color: white; font: 70px Elephant;}')
 
+        # Second Button
+        self.pushButton_2 = QtWidgets.QPushButton(Application)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setStyleSheet('QPushButton {font: 25pt Elephant}')
+        self.gridLayout.addWidget(self.pushButton_2, 3, 0, 1, 1)
+
+        # Third Button
+        self.pushButton_3 = QtWidgets.QPushButton(Application)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
+        self.pushButton_3.setSizePolicy(sizePolicy)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setStyleSheet('QPushButton {font: 25pt Elephant}')
+        self.gridLayout.addWidget(self.pushButton_3, 4, 0, 1, 1)
+
+        # Fourth Button
         self.pushButton_4 = QtWidgets.QPushButton(Application)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
+        self.pushButton_4.setSizePolicy(sizePolicy)
         self.pushButton_4.setObjectName("pushButton_4")
-        self.gridLayout.addWidget(self.pushButton_4, 3, 0, 1, 1)
-        self.pushButton_4.setStyleSheet('QPushButton {background-color: #525151; color: white; font: 70px Elephant;}')
+        self.pushButton_4.setStyleSheet('QPushButton {font: 25pt Elephant}')
+        self.gridLayout.addWidget(self.pushButton_4, 5, 0, 1, 1)
+
+        # Lines
+        self.line = QtWidgets.QFrame(Application)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.gridLayout.addWidget(self.line, 1, 0, 1, 1)
+
+        self.line_2 = QtWidgets.QFrame(Application)
+        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line")
+        self.gridLayout.addWidget(self.line_2, 6, 0, 1, 1)
 
         self.retranslateUi(Application)
-        QtCore.QObject.connect(self.pushButton_3, QtCore.SIGNAL("clicked()"), self.createPortfolioWindow)
         QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL("clicked()"), self.buySellStocks)
         QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL("clicked()"), self.viewPortfolioWindow)
+        QtCore.QObject.connect(self.pushButton_3, QtCore.SIGNAL("clicked()"), self.createPortfolioWindow)
         QtCore.QObject.connect(self.pushButton_4, QtCore.SIGNAL("clicked()"), self.plotStockHistory)
-
         QtCore.QMetaObject.connectSlotsByName(Application)
 
-    # ---------------------------------------------------------------------------------------------------------------- #
+    # ------------------------------------------------------------------------------------------------------------ #
     def retranslateUi(self, Application):
-        Application.setWindowTitle(
-            QtWidgets.QApplication.translate("Application", "Stock Market Application", None, -1))
-        self.pushButton_3.setText(QtWidgets.QApplication.translate("Application", "Create Portfolio", None, -1))
+        Application.setWindowTitle(QtWidgets.QApplication.translate("Application", "Virtual Stock Market Application", None, -1))
         self.pushButton.setText(QtWidgets.QApplication.translate("Application", "Buy/Sell Stocks", None, -1))
-        self.pushButton_2.setText(QtWidgets.QApplication.translate("Application", "View Portfolio", None, -1))
+        self.pushButton_3.setText(QtWidgets.QApplication.translate("Application", "Create Portfolio", None, -1))
+        self.label.setText(
+            QtWidgets.QApplication.translate("Application", "Virtual Stock Market Application", None, -1))
         self.pushButton_4.setText(QtWidgets.QApplication.translate("Application", "Plot Stock History", None, -1))
+        self.pushButton_2.setText(QtWidgets.QApplication.translate("Application", "View Portfolio", None, -1))
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -192,7 +227,7 @@ def main():
     palette.setColor(QPalette.ToolTipBase, Qt.white)
     palette.setColor(QPalette.ToolTipText, Qt.white)
     palette.setColor(QPalette.Text, Qt.white)
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.Button, QColor(82, 81, 81))  # HEX 525151
     palette.setColor(QPalette.ButtonText, Qt.white)
     palette.setColor(QPalette.BrightText, Qt.red)
     palette.setColor(QPalette.Link, QColor(42, 130, 218))
